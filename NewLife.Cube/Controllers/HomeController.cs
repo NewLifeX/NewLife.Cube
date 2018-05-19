eb85f8bfa53f52af39c4d2cf817576f7b7448377
大石头 <nnhy@vip.qq.com>
2018-05-20 01:12:49 +0800

    魔方独立版本，独立发布

---
+++ NewLife.Cube/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+
+namespace NewLife.Cube.Controllers
+{
+    /// <summary>主页面</summary>
+    //[AllowAnonymous]
+    public class CubeHomeController : Controller
+    {
+        /// <summary>主页面</summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            ViewBag.Message = "主页面";
+
+            return View();
+        }
+    }
+}
