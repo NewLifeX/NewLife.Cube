1d8e4b422ecff5bd538e5e432517e8e868e630eb
xxred <1250407685@qq.com>
2018-05-26 02:11:22 +0800

    新建core项目

---
+++ NewLife.CubeNC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NewLife.CubeNC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
