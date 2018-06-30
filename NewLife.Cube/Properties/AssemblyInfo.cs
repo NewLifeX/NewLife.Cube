81ff35756fd72f3474f075b60581cdf23f3a79b8
大石头 <nnhy@vip.qq.com>
2018-06-30 13:10:30 +0800

    v2.4.2018.0630   OAuthServer支持回调地址和密钥验证

--- NewLife.Cube/Properties/AssemblyInfo.cs
+++ NewLife.Cube/Properties/AssemblyInfo.cs
@@ -36,10 +36,12 @@ using NewLife.Cube;
 //
 // 可以指定所有这些值，也可以使用“修订号”和“内部版本号”的默认值，
 // 方法是按如下所示使用“*”:
-[assembly: AssemblyVersion("2.3.*")]
-[assembly: AssemblyFileVersion("2.3.2018.0403")]
+[assembly: AssemblyVersion("2.4.*")]
+[assembly: AssemblyFileVersion("2.4.2018.0630")]
 
 /*
+ * v2.4.2018.0630   OAuthServer支持回调地址和密钥验证
+ * 
  * v2.3.2018.0403   支持数据权限，控制器重载ValidPermission
  * 
  * v2.2.2018.0326   重构权限体系，支持多角色
