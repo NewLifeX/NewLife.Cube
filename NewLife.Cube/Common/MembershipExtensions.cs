6136e6640e0e8dcd60e031ac347bcfa639465939
大石头 <nnhy@vip.qq.com>
2018-08-19 16:47:04 +0800

    共用MembershipExtensions

--- NewLife.Cube/Common/MembershipExtensions.cs
+++ NewLife.Cube/Common/MembershipExtensions.cs
@@ -1,6 +1,10 @@
 ﻿using System;
-using System.Web.Mvc;
 using XCode.Membership;
+#if __CORE__
+using Microsoft.AspNetCore.Mvc.Razor;
+#else
+using System.Web.Mvc;
+#endif
 
 namespace NewLife.Cube
 {
@@ -67,6 +71,24 @@ namespace NewLife.Cube
             return false;
         }
 
+#if __CORE__
+        /// <summary>用户只有拥有当前菜单的指定权限</summary>
+        /// <param name="page">页面</param>
+        /// <param name="flags">是否拥有多个权限中的任意一个，或的关系。如果需要表示与的关系，可以传入一个多权限位合并</param>
+        /// <returns></returns>
+        public static Boolean Has(this IRazorPage page, params PermissionFlags[] flags)
+        {
+            // 没有用户时无权
+            var user = page.ViewContext.ViewBag.User as IUser ?? page.ViewContext.HttpContext.User.Identity as IUser;
+            if (user == null) return false;
+
+            // 没有菜单时不做权限控制
+            var menu = page.ViewContext.ViewBag.Menu as IMenu;
+            if (menu == null) return true;
+
+            return user.Has(menu, flags);
+        }
+#else
         /// <summary>用户只有拥有当前菜单的指定权限</summary>
         /// <param name="page">页面</param>
         /// <param name="flags">是否拥有多个权限中的任意一个，或的关系。如果需要表示与的关系，可以传入一个多权限位合并</param>
@@ -83,5 +105,6 @@ namespace NewLife.Cube
 
             return user.Has(menu, flags);
         }
+#endif
     }
 }
