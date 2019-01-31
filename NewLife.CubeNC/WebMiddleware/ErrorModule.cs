85343a5f0df93e5b098bb842919d332766841a80
xxred <xxred0324@qq.com>
2019-02-01 02:59:29 +0800

    完成错误异常处理中间件，修改判断ajax请求方法

--- NewLife.CubeNC/WebMiddleware/ErrorModule.cs
+++ NewLife.CubeNC/WebMiddleware/ErrorModule.cs
@@ -1,7 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Diagnostics.RazorViews;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Abstractions;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Routing;
+using NewLife.Cube.Extensions;
+using NewLife.Log;
+using NewLife.Serialization;
 
 namespace NewLife.Cube.WebMiddleware
 {
@@ -11,21 +24,76 @@ namespace NewLife.Cube.WebMiddleware
 
         public ErrorModuleMiddleware(RequestDelegate next)
         {
-            if (next == null)
-            {
-                throw new ArgumentNullException(nameof(next));
-            }
-
-            _next = next;
+            _next = next ?? throw new ArgumentNullException(nameof(next));
         }
 
-        public async Task Invoke(HttpContext context)
+        public async Task Invoke(HttpContext context, IRazorViewEngine viewEngine)
         {
-            // Do something with context near the beginning of request processing.
+            try
+            {
+                await _next.Invoke(context);
+            }
+            catch (Exception e)
+            {
+                if (context.Response.HasStarted)
+                {
+                    XTrace.WriteException(e);
+                    throw;
+                }
 
-            await _next.Invoke(context);
+                context.Response.Clear();
+                context.Response.StatusCode = 500;
+                await ExceptionHandler(context, e, viewEngine);
+            }
+        }
 
-            // Clean up.
+        /// <summary>处理异常</summary>
+        /// <param name="context"></param>
+        private async Task ExceptionHandler(HttpContext context, Exception ex, IRazorViewEngine viewEngine)
+        {
+            //判断api请求还是页面请求
+            if (context.Request.IsAjaxRequest())
+            {
+                var data = ex.GetTrue().Message;
+                var rs = new { result = false, data };
+                var json = rs.ToJson();
+                var bytes = json.GetBytes();
+                await context.Response.Body.WriteAsync(bytes);
+                context.Response.ContentLength = bytes.Length;
+            }
+            else
+            {
+                var viewName = "CubeError";
+                var viewEngineResult = viewEngine.FindView(new ActionContext()
+                {
+                    HttpContext = context,
+                    ActionDescriptor = new ActionDescriptor(),
+                    RouteData = new RouteData()
+                }, viewName, false);
+
+                if (!viewEngineResult.Success)
+                {
+                    throw new ArgumentNullException($"没有找到名为{viewName}的视图文件");
+                }
+
+                using (var sw = new StringWriter())
+                {
+                    var viewContext = new ViewContext()
+                    {
+                        HttpContext = context,
+                        Writer = sw,
+                        ViewData = new ViewDataDictionary<Exception>(new EmptyModelMetadataProvider(), new ModelStateDictionary())
+                        {
+                            { "Exception", ex }
+                        }
+                    };
+
+                    await viewEngineResult.View.RenderAsync(viewContext);
+                    var bytes = sw.ToString().GetBytes();
+                    await context.Response.Body.WriteAsync(bytes);
+                    context.Response.ContentLength = bytes.Length;
+                }
+            }
         }
     }
 
