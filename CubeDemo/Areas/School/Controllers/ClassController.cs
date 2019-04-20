58ea779c2840107e01936cc36f45c07e8b9e8630
大石头 <nnhy@vip.qq.com>
2019-04-20 23:35:38 +0800

    支持批量生成模拟数据

--- CubeDemo/Areas/School/Controllers/ClassController.cs
+++ CubeDemo/Areas/School/Controllers/ClassController.cs
@@ -1,13 +1,41 @@
-﻿using NewLife.Cube;
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using NewLife.Cube;
 using NewLife.School.Entity;
+using NewLife.Security;
+using NewLife.Web;
+using XCode;
 
 namespace CubeDemo.Areas.School.Controllers
 {
     public class ClassController : EntityController<Class>
     {
-        static ClassController()
+        static ClassController() => ListFields.RemoveField("Remark");
+
+        public ActionResult MakeData()
         {
-            ListFields.RemoveField("Remark");
+            // 关闭日志
+            Class.Meta.Session.Dal.Db.ShowSQL = false;
+
+            var count = 1_000_000;
+            var list = new List<Class>();
+            for (var i = 0; i < count; i++)
+            {
+                var entity = new Class
+                {
+                    Name = Rand.NextString(8)
+                };
+
+                list.Add(entity);
+
+                if ((i + 1) % 5000 == 0)
+                {
+                    list.Insert();
+                    list.Clear();
+                }
+            }
+
+            return IndexView(new Pager());
         }
     }
 }
--- CubeDemo/Areas/School/Controllers/StudentController.cs
+++ CubeDemo/Areas/School/Controllers/StudentController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Web.Mvc;
 using NewLife.Cube;
 using NewLife.School.Entity;
+using NewLife.Security;
 using NewLife.Web;
+using XCode;
 using XCode.Membership;
 
 namespace CubeDemo.Areas.School.Controllers
@@ -16,6 +19,32 @@ namespace CubeDemo.Areas.School.Controllers
             var list = Student.Search(SexKinds.女, "992班", p);
 
             return Student.Search(classid, p["q"], p);
+        }
+
+        public ActionResult MakeData()
+        {
+            // 关闭日志
+            Student.Meta.Session.Dal.Db.ShowSQL = false;
+
+            var count = 1_000_000;
+            var list = new List<Student>();
+            for (var i = 0; i < count; i++)
+            {
+                var entity = new Student
+                {
+                    Name = Rand.NextString(8)
+                };
+
+                list.Add(entity);
+
+                if ((i + 1) % 5000 == 0)
+                {
+                    list.Insert();
+                    list.Clear();
+                }
+            }
+
+            return IndexView(new Pager());
         }
     }
 }
