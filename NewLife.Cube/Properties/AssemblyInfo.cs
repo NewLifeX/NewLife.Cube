dc3e60e34a4aea35bc7abb48e8426e395e47726d
大石头 <nnhy@vip.qq.com>
2019-06-03 00:21:58 +0800

    发布0602，v2.8.2019.0602   支持备份与恢复数据

--- NewLife.Cube/Properties/AssemblyInfo.cs
+++ NewLife.Cube/Properties/AssemblyInfo.cs
@@ -36,10 +36,12 @@ using NewLife.Cube;
 //
 // 可以指定所有这些值，也可以使用“修订号”和“内部版本号”的默认值，
 // 方法是按如下所示使用“*”:
-[assembly: AssemblyVersion("2.7.*")]
-[assembly: AssemblyFileVersion("2.7.2019.0406")]
+[assembly: AssemblyVersion("2.8.*")]
+[assembly: AssemblyFileVersion("2.8.2019.0602")]
 
 /*
+ * v2.8.2019.0602   支持备份与恢复数据
+ * 
  * v2.7.2019.0406   拆分只读实体控制器，用于只读数据展示
  * 
  * v2.6.2019.0221   列表页尾部支持增加汇总统计行
