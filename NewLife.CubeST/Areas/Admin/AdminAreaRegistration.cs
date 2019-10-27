e8deb7d7117cd805082a12e3d6f7ef6ef458ed98
郗云飞 <xiyunfei@zto.cn>
2019-10-27 20:59:52 +0800

    增加3.0项目

---
+++ NewLife.CubeST/Areas/Admin/AdminAreaRegistration.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel;
+using NewLife.Log;
+using XCode.Membership;
+
+namespace NewLife.Cube.Admin
+{
+    /// <summary>权限管理区域注册</summary>
+    [DisplayName("系统管理")]
+    public class AdminArea : AreaBase
+    {
+        /// <summary>区域名</summary>
+        public static String AreaName => nameof(AdminArea).TrimEnd("Area");
+
+        /// <inheritdoc />
+        public AdminArea() : base(AreaName)
+        {
+           
+        }
+
+        static AdminArea()
+        {
+            // 自动检查并添加菜单
+            XTrace.WriteLine("初始化权限管理体系");
+            //var user = ManageProvider.User;
+            ManageProvider.Provider.GetService<IUser>();
+
+            RegisterArea<AdminArea>();
+        }
+    }
+}
---
+++ NewLife.CubeST/Common/CubeLogger.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using XCode.Membership;
+
+namespace NewLife.Cube.Common
+{
+    /// <summary>
+    /// 在这里可以做实时日志，每次请求都会有详细信息输出
+    /// </summary>
+    public class CubeLogger : ILogger
+    {
+        public CubeLogger()
+        {
+        }
+
+        public virtual void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+        {
+            var remark = formatter?.Invoke(state, exception) ?? state.ToString();
+
+            // 忽略读取session的日志，避免下面WriteLog触发系统写日志操作导致无限循环写日志StackOverflowException
+            if (remark.StartsWith("Accessing expired session"))
+            {
+                return;
+            }
+
+            WriteLog<TState>(eventId.ToString(), remark);
+        }
+
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            if (Setting.Current.Debug)
+            {
+                return true;
+            }
+
+            if (logLevel > LogLevel.Warning)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public IDisposable BeginScope<TState>(TState state)
+        {
+            return null;
+        }
+
+        private void WriteLog<TState>(String action, String remark, String ip = null)
+        {
+            // 此处记录到了审计日志，造成了污染。
+            // 实际上每个请求的相关信息都会记录到框架自带的log，这个文件的记录只是其中一个，用来输出
+            // 比如通过webstock实时反馈到web端，方便调试或查看请求过程
+            //LogProvider.Provider.WriteLog(typeof(TState), action, remark, ip: ip);
+        }
+    }
+
+    public class CubeLoggerProvider : ILoggerProvider
+    {
+        public void Dispose()
+        {
+        }
+
+        public ILogger CreateLogger(string categoryName) => new CubeLogger();
+    }
+}
