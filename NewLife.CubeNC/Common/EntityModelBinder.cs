e03c4c8822897f7f5191b3cb72bccd6e85d62ce8
大石头 <nnhy@vip.qq.com>
2019-12-04 17:13:29 +0800

    修正实体模型绑定器无法准确填充内部字段的问题

--- NewLife.CubeNC/Common/EntityModelBinder.cs
+++ NewLife.CubeNC/Common/EntityModelBinder.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using NewLife.Cube.Extensions;
@@ -14,12 +15,13 @@ using HttpContext = NewLife.Web.HttpContext;
 namespace NewLife.Cube
 {
     /// <summary>实体模型绑定器</summary>
-    public class EntityModelBinder : IModelBinder
+    public class EntityModelBinder : ComplexTypeModelBinder
     {
         /// <summary>实例化实体模型绑定器</summary>
         /// <param name="propertyBinders"></param>
         /// <param name="loggerFactory"></param>
         public EntityModelBinder(IDictionary<ModelMetadata, IModelBinder> propertyBinders, ILoggerFactory loggerFactory)
+            : base(propertyBinders, loggerFactory)
         {
             _propertyBinders = propertyBinders ?? throw new ArgumentNullException(nameof(propertyBinders));
             _loggerFactory = loggerFactory ?? throw new ArgumentNullException(nameof(loggerFactory));
@@ -28,19 +30,94 @@ namespace NewLife.Cube
         private readonly IDictionary<ModelMetadata, IModelBinder> _propertyBinders;
         private readonly ILoggerFactory _loggerFactory;
 
-        /// <summary>异步绑定模型</summary>
+        ///// <summary>异步绑定模型</summary>
+        ///// <param name="bindingContext"></param>
+        ///// <returns></returns>
+        //public async Task BindModelAsync(ModelBindingContext bindingContext)
+        //{
+        //    var modelType = bindingContext.ModelType;
+        //    var controllerContext = bindingContext.ActionContext;
+        //    if (modelType.As<IEntity>())
+        //    {
+        //        var fact = EntityFactory.CreateOperate(modelType);
+        //        if (fact != null)
+        //        {
+        //            var rvs = controllerContext.RouteData.Values;
+        //            var pks = fact.Table.PrimaryKeys;
+        //            var uk = fact.Unique;
+
+        //            IEntity entity = null;
+        //            if (uk != null)
+        //            {
+        //                // 查询实体对象用于编辑
+        //                if (rvs[uk.Name] != null) entity =
+        //                    //GetEntity(fact.EntityType, rvs[uk.Name]) ?? 
+        //                    fact.FindByKeyForEdit(rvs[uk.Name]); // 从session取回来的实体全部被设置了脏属性，每次保存所有数据，因此从数据查找
+        //                if (entity == null) entity = fact.Create();
+        //            }
+        //            else if (pks.Length > 0)
+        //            {
+        //                // 查询实体对象用于编辑
+        //                var vs = pks.Select(e => rvs[e.Name]).ToArray();
+        //                entity = GetEntity(fact.EntityType, vs);
+        //                if (entity == null)
+        //                {
+        //                    var req = controllerContext.HttpContext.Request;
+        //                    var exp = new WhereExpression();
+        //                    foreach (var item in pks)
+        //                    {
+        //                        exp &= item.Equal(req.GetRequestValue(item.Name).ChangeType(item.Type));
+        //                    }
+
+        //                    entity = fact.Find(exp);
+        //                }
+        //                if (entity == null) entity = fact.Create();
+        //            }
+
+        //            if (entity != null)
+        //            {
+        //                var req = controllerContext.HttpContext.Request;
+        //                if (req.HasFormContentType)
+        //                {
+        //                    var fs = req.Form;
+        //                    // 提前填充动态字段的扩展属性
+        //                    foreach (var item in fact.Fields)
+        //                    {
+        //                        if (item.IsDynamic && fs.ContainsKey(item.Name)) entity.SetItem(item.Name, fs[item.Name]);
+        //                    }
+        //                }
+
+        //                bindingContext.Result = ModelBindingResult.Success(entity);
+        //            }
+
+        //            if (entity == null)
+        //            {
+        //                bindingContext.Result = ModelBindingResult.Success(fact.Create());
+        //            }
+
+        //            // 为Model赋值，为下面BindProperty方法做准备
+        //            bindingContext.Model = bindingContext.Result.Model;
+
+        //            // 使用复杂类型模型绑定器ComplexTypeModelBinder填充Model
+        //            await BindProperty(bindingContext);
+        //        }
+        //    }
+
+        //    //return Task.CompletedTask;
+        //}
+
+        /// <summary>创建模型。对于有Key的请求，使用FindByKeyForEdit方法先查出来数据，而不是直接反射实例化实体对象</summary>
         /// <param name="bindingContext"></param>
         /// <returns></returns>
-        public Task BindModelAsync(ModelBindingContext bindingContext)
+        protected override Object CreateModel(ModelBindingContext bindingContext)
         {
             var modelType = bindingContext.ModelType;
-            var controllerContext = bindingContext.ActionContext;
             if (modelType.As<IEntity>())
             {
                 var fact = EntityFactory.CreateOperate(modelType);
                 if (fact != null)
                 {
-                    var rvs = controllerContext.RouteData.Values;
+                    var rvs = bindingContext.ActionContext.RouteData.Values;
                     var pks = fact.Table.PrimaryKeys;
                     var uk = fact.Unique;
 
@@ -48,9 +125,7 @@ namespace NewLife.Cube
                     if (uk != null)
                     {
                         // 查询实体对象用于编辑
-                        if (rvs[uk.Name] != null) entity =
-                            //GetEntity(fact.EntityType, rvs[uk.Name]) ?? 
-                            fact.FindByKeyForEdit(rvs[uk.Name]); // 从session取回来的实体全部被设置了脏属性，每次保存所有数据，因此从数据查找
+                        if (rvs[uk.Name] != null) entity = GetEntity(fact.EntityType, rvs[uk.Name]) ?? fact.FindByKeyForEdit(rvs[uk.Name]);
                         if (entity == null) entity = fact.Create();
                     }
                     else if (pks.Length > 0)
@@ -60,11 +135,11 @@ namespace NewLife.Cube
                         entity = GetEntity(fact.EntityType, vs);
                         if (entity == null)
                         {
-                            var req = controllerContext.HttpContext.Request;
+                            var req = bindingContext.HttpContext.Request.Query;
                             var exp = new WhereExpression();
                             foreach (var item in pks)
                             {
-                                exp &= item.Equal(req.GetRequestValue(item.Name).ChangeType(item.Type));
+                                exp &= item.Equal(req[item.Name].ChangeType(item.Type));
                             }
 
                             entity = fact.Find(exp);
@@ -74,35 +149,21 @@ namespace NewLife.Cube
 
                     if (entity != null)
                     {
-                        var req = controllerContext.HttpContext.Request;
-                        if (req.HasFormContentType)
+                        var fs = bindingContext.HttpContext.Request.Form;
+                        // 提前填充动态字段的扩展属性
+                        foreach (var item in fact.Fields)
                         {
-                            var fs = req.Form;
-                            // 提前填充动态字段的扩展属性
-                            foreach (var item in fact.Fields)
-                            {
-                                if (item.IsDynamic && fs.ContainsKey(item.Name)) entity.SetItem(item.Name, fs[item.Name]);
-                            }
+                            if (item.IsDynamic && fs.ContainsKey(item.Name)) entity.SetItem(item.Name, fs[item.Name]);
                         }
 
-
-                        bindingContext.Result = ModelBindingResult.Success(entity);
+                        return entity;
                     }
 
-                    if (entity == null)
-                    {
-                        bindingContext.Result = ModelBindingResult.Success(fact.Create());
-                    }
-
-                    // 为Model赋值，为下面BindProperty方法做准备
-                    bindingContext.Model = bindingContext.Result.Model;
-
-                    //// 使用复杂类型模型绑定器ComplexTypeModelBinder填充Model
-                    //await BindProperty(bindingContext);
+                    return fact.Create();
                 }
             }
 
-            return Task.CompletedTask;
+            return base.CreateModel(bindingContext);
         }
 
         private static String GetCacheKey(Type type, params Object[] keys)
