115fe254c6559fb2b516bfcae80aed575d4b9734
大石头 <nnhy@vip.qq.com>
2019-12-07 23:50:49 +0800

    修正IP地址显示

--- CubeDemoNC/Program.cs
+++ CubeDemoNC/Program.cs
@@ -33,6 +33,7 @@ namespace CubeDemoNC
                 .ConfigureLogging(logging => { logging.AddXLog(); })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    webBuilder.UseUrls("http://*:5000;https://*:5001");
                     webBuilder.UseStartup<Startup>();
                 });
         }
