6a57182358107b5f69c0445193e47f0fd256174d
智能石头 <nnhy@vip.qq.com>
2020-12-16 18:08:16 +0800

    加长应用日志的回调地址字段

--- NewLife.Cube/Entity/应用日志.cs
+++ NewLife.Cube/Entity/应用日志.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Runtime.Serialization;
@@ -62,7 +62,7 @@ namespace NewLife.Cube.Entity
         /// <summary>回调地址</summary>
         [DisplayName("回调地址")]
         [Description("回调地址")]
-        [DataObjectField(false, false, true, 200)]
+        [DataObjectField(false, false, true, 500)]
         [BindColumn("RedirectUri", "回调地址", "")]
         public String RedirectUri { get => _RedirectUri; set { if (OnPropertyChanging("RedirectUri", value)) { _RedirectUri = value; OnPropertyChanged("RedirectUri"); } } }
 
