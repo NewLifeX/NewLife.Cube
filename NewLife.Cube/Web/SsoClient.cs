cd993e288a15bc0cd62b778a74c27763f8210f72
智能石头 <nnhy@vip.qq.com>
2021-01-25 09:48:08 +0800

    SsoClient  单元测试

--- NewLife.Cube/Web/SsoClient.cs
+++ NewLife.Cube/Web/SsoClient.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using NewLife.Log;
 using NewLife.Model;
 using NewLife.Remoting;
+using NewLife.Serialization;
 using XCode.Membership;
 
 namespace NewLife.Cube.Web
@@ -22,7 +23,7 @@ namespace NewLife.Cube.Web
         /// <summary>应用密钥</summary>
         public String Secret { get; set; }
 
-        private HttpClient _client = DefaultTracer.Instance.CreateHttpClient();
+        private HttpClient _client;
         #endregion
 
         private HttpClient GetClient()
@@ -39,11 +40,11 @@ namespace NewLife.Cube.Web
         /// <param name="username"></param>
         /// <param name="password"></param>
         /// <returns></returns>
-        public async Task<String> GetToken(String username, String password)
+        public async Task<IDictionary<String, Object>> GetTokenInfo(String username, String password)
         {
             var client = GetClient();
 
-            var rs = await client.GetAsync<IDictionary<String, Object>>("sso/token", new
+            return await client.GetAsync<IDictionary<String, Object>>("sso/token", new
             {
                 grant_type = "password",
                 client_id = AppId,
@@ -51,6 +52,15 @@ namespace NewLife.Cube.Web
                 username,
                 password
             });
+        }
+
+        /// <summary>验证账号密码，并返回令牌</summary>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        public async Task<String> GetToken(String username, String password)
+        {
+            var rs = await GetTokenInfo(username, password);
 
             return rs["access_token"] as String;
         }
@@ -58,11 +68,28 @@ namespace NewLife.Cube.Web
         /// <summary>通过令牌获取用户信息</summary>
         /// <param name="accessToken"></param>
         /// <returns></returns>
-        public async Task<IManageUser> GetUser(String accessToken)
+        public async Task<IDictionary<String, Object>> GetUserInfo(String accessToken)
         {
             var client = GetClient();
 
-            return await client.GetAsync<User>("sso/userinfo", new { access_token = accessToken });
+            return await client.GetAsync<IDictionary<String, Object>>("sso/userinfo", new { access_token = accessToken });
+        }
+
+        /// <summary>通过令牌获取用户信息</summary>
+        /// <param name="accessToken"></param>
+        /// <returns></returns>
+        public async Task<IManageUser> GetUser(String accessToken)
+        {
+            var dic = await GetUserInfo(accessToken);
+
+            var user = JsonHelper.Convert<User>(dic);
+
+            if (dic.TryGetValue("userid", out var str)) user.ID = str.ToInt();
+            if (dic.TryGetValue("usercode", out str)) user.Code = str as String;
+            if (dic.TryGetValue("username", out str)) user.Name = str as String;
+            if (dic.TryGetValue("nickname", out str)) user.DisplayName = str as String;
+
+            return user;
         }
     }
 }
---
+++ XUnitTest/SsoClientTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NewLife.Cube.Web;
+using NewLife.Log;
+using XCode.Membership;
+using Xunit;
+
+namespace XUnitTest
+{
+    public class SsoClientTests
+    {
+        [Fact]
+        public async void Test()
+        {
+            var client = new SsoClient
+            {
+                Server = "https://sso.newlifex.com",
+                AppId = "NewLife.Cube",
+                Secret = "",
+            };
+
+            var token = await client.GetToken("admin", "admin");
+            Assert.NotEmpty(token);
+            XTrace.WriteLine(token);
+
+            var user = await client.GetUser(token) as User;
+            Assert.NotNull(user);
+            Assert.Equal(1, user.ID);
+            Assert.Equal("admin", user.Name);
+            Assert.Equal("管理员", user.DisplayName);
+        }
+    }
+}
