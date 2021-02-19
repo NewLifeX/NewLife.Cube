f3548989b37320ec11c57763bec14d49a1c563ad
大石头 <nnhy@vip.qq.com>
2021-02-19 23:27:55 +0800

    新增模型表和模型字段管理，用于前端定制

---
+++ NewLife.Cube/Areas/Admin/Controllers/ModelColumnController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Microsoft.AspNetCore.Mvc;
+using NewLife.Cube.Entity;
+using XCode;
+using XCode.Membership;
+
+namespace NewLife.Cube.Admin.Controllers
+{
+    /// <summary>模型列</summary>
+    [Area("Admin")]
+    public class ModelColumnController : EntityController<ModelColumn>
+    {
+        static ModelColumnController() => MenuOrder = 56;
+
+        /// <summary>菜单不可见</summary>
+        /// <param name="menu"></param>
+        /// <returns></returns>
+        protected override IDictionary<MethodInfo, Int32> ScanActionMenu(IMenu menu)
+        {
+            if (menu.Visible && !menu.Necessary)
+            {
+                menu.Visible = false;
+                (menu as IEntity).Update();
+            }
+
+            return base.ScanActionMenu(menu);
+        }
+    }
+}
