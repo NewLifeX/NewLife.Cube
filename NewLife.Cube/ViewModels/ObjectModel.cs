f82ffdf041d8eb54babd4ebcc4be26e3873404da
笑笑 <xxred0324@qq.com>
2021-03-08 23:39:39 +0800

    GetColumns格式化Name的值

--- NewLife.Cube/ViewModels/ObjectModel.cs
+++ NewLife.Cube/ViewModels/ObjectModel.cs
@@ -26,7 +26,7 @@ namespace NewLife.Cube.ViewModels
 
         public String Name
         {
-            get => FormatName(_name);
+            get => _name.FormatName(FormatType);
             set => _name = value;
         }
 
@@ -35,19 +35,5 @@ namespace NewLife.Cube.ViewModels
         [IgnoreDataMember]
         public Type PropertyType { get; set; }
         public String TypeStr { get; set; }
-
-        /// <summary>根据小写和驼峰格式化名称</summary>
-        /// <param name="name"></param>
-        /// <returns></returns>
-        private String FormatName(String name)
-        {
-            if (name.IsNullOrEmpty()) return name;
-
-            if (FormatType == FormatType.LowerCase) return name.ToLower();
-            if (FormatType!= FormatType.CamelCase) return name;
-            if (name.EqualIgnoreCase("id")) return "id";
-            if (name.Length < 2) return name.ToLower();
-            return name.Substring(0, 1).ToLower() + name.Substring(1);
-        }
     }
 }
