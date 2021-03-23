2cf189b1082951509cb4c43fddc531e77032357d
大石头 <nnhy@vip.qq.com>
2021-03-23 09:03:09 +0800

    修正微信登录

--- NewLife.Cube/Web/OAuth/WeixinClient.cs
+++ NewLife.Cube/Web/OAuth/WeixinClient.cs
@@ -9,7 +9,7 @@ namespace NewLife.Web.OAuth
         /// <summary>实例化</summary>
         public WeixinClient()
         {
-            Server = "https://open.weixin.qq.com/connect/oauth2/";
+            Server = "https://open.weixin.qq.com/connect/";
 
             AuthUrl = "qrconnect?response_type={response_type}&appid={key}&redirect_uri={redirect}&state={state}&scope={scope}";
             AccessUrl = "https://api.weixin.qq.com/sns/oauth2/access_token?grant_type=authorization_code&appid={key}&secret={secret}&code={code}";
@@ -35,17 +35,17 @@ namespace NewLife.Web.OAuth
             {
                 // 扫码登录
                 case "snsapi_login":
-                    AuthUrl = "qrconnect?response_type={response_type}&appid={key}&redirect_uri={redirect}&state={state}&scope={scope}";
+                    AuthUrl = "qrconnect?response_type={response_type}&appid={key}&redirect_uri={redirect}&state={state}&scope={scope}#wechat_redirect";
                     Scope = mode;
                     break;
                 // 静默授权，用户无感知
                 case "snsapi_base":
-                    AuthUrl = "authorize?response_type={response_type}&appid={key}&redirect_uri={redirect}&state={state}&scope={scope}#wechat_redirect";
+                    AuthUrl = "oauth2/authorize?response_type={response_type}&appid={key}&redirect_uri={redirect}&state={state}&scope={scope}#wechat_redirect";
                     Scope = mode;
                     break;
                 // 授权需要用户手动同意
                 case "snsapi_userinfo":
-                    AuthUrl = "authorize?response_type={response_type}&appid={key}&redirect_uri={redirect}&state={state}&scope={scope}#wechat_redirect";
+                    AuthUrl = "oauth2/authorize?response_type={response_type}&appid={key}&redirect_uri={redirect}&state={state}&scope={scope}#wechat_redirect";
                     Scope = mode;
                     break;
             }
@@ -58,17 +58,17 @@ namespace NewLife.Web.OAuth
             !userAgent.IsNullOrEmpty() &&
             (userAgent.Contains(" MicroMessenger/") || userAgent.Contains(" MICROMESSENGER/"));
 
-        ///// <summary>针对指定客户端进行初始化</summary>
-        ///// <param name="userAgent"></param>
-        //public override void Init(String userAgent)
-        //{
-        //    // 应用内打开时，自动切换为应用内免登
-        //    if (Support(userAgent))
-        //    {
-        //        Scope = "snsapi_userinfo";
-        //        SetMode(Scope);
-        //    }
-        //}
+        /// <summary>针对指定客户端进行初始化</summary>
+        /// <param name="userAgent"></param>
+        public override void Init(String userAgent)
+        {
+            // 应用内打开时，自动切换为应用内免登
+            if (Support(userAgent))
+            {
+                Scope = "snsapi_base";
+                SetMode(Scope);
+            }
+        }
 
         /// <summary>从响应数据中获取信息</summary>
         /// <param name="dic"></param>
