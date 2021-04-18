3348bb3226ee2a7d495a1658cdfb40a67cbfc397
笑笑 <xxred0324@qq.com>
2021-04-18 16:59:59 +0800

    启用新ui时不拦截带参数的页面请求、完善注释

--- CubeDemoNC/Startup.cs
+++ CubeDemoNC/Startup.cs
@@ -57,9 +57,16 @@ namespace CubeDemoNC
             //app.UseHttpsRedirection();
             app.UseStaticFiles();
 
+            /* 新ui通道，以下需同时满足条件
+             * 1、配置启用新ui
+             * 2、GET请求
+             * 3、非json请求
+             * 4、不带query参数。如果真的是新ui的页面请求，且带了query参数，不能与魔方旧有接口一样，否则不会命中
+             * */
             app.UseWhen(
                 context => set.EnableNewUI && context.Request.Method.EqualIgnoreCase("GET") &&
-                           !context.Request.IsAjaxRequest(),
+                           !context.Request.IsAjaxRequest() &&
+                           context.Request.Query.Count < 1,
                 a =>
                 {
                     var staticFileOptions = new StaticFileOptions()
@@ -104,6 +111,31 @@ namespace CubeDemoNC
             //    var client = new LocalStarClient();
             //    client.ProbeAndInstall(null, "1.1");
             //});
+
+            // 所有请求没有命中的，统一在这里处理
+            app.UseWhen(context => set.EnableNewUI,
+                a =>
+                {
+                    var staticFileOptions = new StaticFileOptions()
+                    {
+                        FileProvider = new PhysicalFileProvider(env.WebRootPath),
+                        OnPrepareResponse = (context =>
+                        {
+                            if (!context.Context.Response.Headers[HeaderNames.ContentType].Contains("text/html"))
+                                return;
+                            context.Context.Response.Headers.Remove(HeaderNames.ETag);
+                            context.Context.Response.Headers.Remove(HeaderNames.LastModified);
+                        })
+                    };
+
+                    a.UseDefaultFiles();
+
+                    a.UseSpa(options =>
+                    {
+                        a.UseSpaStaticFiles(staticFileOptions);
+                        options.Options.DefaultPageStaticFileOptions = staticFileOptions;
+                    });
+                });
         }
     }
 }
