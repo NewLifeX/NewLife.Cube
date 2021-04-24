9e26877113fe34d066f5d6a83fd57aa3813be1d2
大石头 <nnhy@vip.qq.com>
2021-04-24 09:40:16 +0800

    设置委托代理人的默认值

--- NewLife.Cube/Areas/Admin/Controllers/PrincipalAgentController.cs
+++ NewLife.Cube/Areas/Admin/Controllers/PrincipalAgentController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.ComponentModel;
+using Microsoft.AspNetCore.Mvc;
 using NewLife.Cube.Entity;
 
 namespace NewLife.Cube.Admin.Controllers
@@ -7,5 +9,16 @@ namespace NewLife.Cube.Admin.Controllers
     [Area("Admin")]
     public class PrincipalAgentController : EntityController<PrincipalAgent>
     {
+        protected override Boolean Valid(PrincipalAgent entity, DataObjectMethodType type, Boolean post)
+        {
+            if (!post && type == DataObjectMethodType.Insert)
+            {
+                entity.Enable = true;
+                entity.Times = 1;
+                entity.Expire = DateTime.Now.AddMinutes(20);
+            }
+
+            return base.Valid(entity, type, post);
+        }
     }
 }
