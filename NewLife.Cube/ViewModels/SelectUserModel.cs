627e5c4eb83063f85696cb6a4149c5efa681a6b6
大石头 <nnhy@vip.qq.com>
2021-04-24 09:54:03 +0800

    选择用户的_SelectUser视图，支持传递更多参数

---
+++ NewLife.Cube/ViewModels/SelectUserModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace NewLife.Cube.ViewModels
+{
+    /// <summary>选择用户控件所使用的模型</summary>
+    public class SelectUserModel
+    {
+        /// <summary>控件</summary>
+        public String Id { get; set; }
+
+        /// <summary>角色</summary>
+        public Int32 RoleId { get; set; }
+
+        /// <summary>部门</summary>
+        public Int32 DepartmentId { get; set; }
+
+        /// <summary>用户Id</summary>
+        public Int32 UserId { get; set; }
+    }
+}
