8c72c81024b64bdaaafbc92b10c9a6993ea657b0
xiyunfei <xi3892@163.com>
2021-06-30 10:45:28 +0800

    完成首页内所有页面的layui界面切换

--- NewLife.CubeNC/Areas/Admin/Index/IndexController.cs
+++ NewLife.CubeNC/Areas/Admin/Index/IndexController.cs
@@ -92,13 +92,40 @@ namespace NewLife.Cube.Admin.Controllers
             return ((id + "").ToLower()) switch
             {
                 "processmodules" => View("ProcessModules"),
-                "assembly" => View("Assembly_layui"),
+                "assembly" => View("Assembly"),
                 "session" => View("Session"),
                 "cache" => View("Cache"),
                 "servervar" => View("ServerVar"),
+                _ => View("Main"),
+            };
+        }
+
+        /// <summary>服务器信息(layui)</summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [DisplayName("服务器信息(layui)")]
+        [EntityAuthorize(PermissionFlags.Detail)]
+        public ActionResult MainLayui(String id)
+        {
+            ViewBag.Act = id;
+            ViewBag.Config = SysConfig.Current;
+            ViewBag.MyAsms = GetMyAssemblies().OrderBy(e => e.Name).OrderByDescending(e => e.Compile).ToArray();
+
+            var Asms = AssemblyX.GetAssemblies(null).ToArray();
+            Asms = Asms.OrderBy(e => e.Name).OrderByDescending(e => e.Compile).ToArray();
+            ViewBag.Asms = Asms;
+
+            return ((id + "").ToLower()) switch
+            {
+                "processmodules" => View("ProcessModules_layui"),
+                "assembly" => View("Assembly_layui"),
+                "session" => View("Session_layui"),
+                "cache" => View("Cache_layui"),
+                "servervar" => View("ServerVar_layui"),
                 _ => View("Main_layui"),
             };
         }
+
 
         /// <summary>获取当前应用程序的所有程序集，不包括系统程序集，仅限本目录</summary>
         /// <returns></returns>
