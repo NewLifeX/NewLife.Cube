47282553a2cd562eff64de5bd4e3d63e36aa0b92
智能大石头 <nnhy@vip.qq.com>
2021-07-15 19:06:15 +0800

    OAuthServer增加黑白名单匹配

--- NewLife.Cube/Web/SsoProvider.cs
+++ NewLife.Cube/Web/SsoProvider.cs
@@ -495,7 +495,7 @@ namespace NewLife.Cube.Web
         /// <returns></returns>
         public virtual TokenInfo GetAccessToken(OAuthServer sso, String client_id, String client_secret, String code, String ip)
         {
-            sso.Auth(client_id, client_secret);
+            sso.Auth(client_id, client_secret, ip);
 
             var token = sso.GetToken(code);
             token.Scope = "basic,UserInfo";
@@ -524,7 +524,7 @@ namespace NewLife.Cube.Web
 
             try
             {
-                var app = sso.Auth(client_id, null);
+                var app = sso.Auth(client_id, null, ip);
                 log.AppId = app.ID;
 
                 // 验证应用能力
@@ -611,7 +611,7 @@ namespace NewLife.Cube.Web
                 var app = App.FindByName(client_id);
                 if (app != null) log.AppId = app.ID;
 
-                app = sso.Auth(client_id, client_secret);
+                app = sso.Auth(client_id, client_secret, ip);
                 log.AppId = app.ID;
 
                 // 验证应用能力
@@ -666,7 +666,7 @@ namespace NewLife.Cube.Web
                 var app = App.FindByName(client_id);
                 if (app != null) log.AppId = app.ID;
 
-                app = sso.Auth(client_id, null);
+                app = sso.Auth(client_id, null, ip);
                 log.AppId = app.ID;
 
                 var name = sso.Decode(refresh_token);
