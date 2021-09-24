7a287719559a447cc7026044184a89193056ed30
大石头 <nnhy@vip.qq.com>
2021-09-24 17:39:41 +0800

    新增微博对接

---
+++ NewLife.Cube/Web/OAuth/WeiboClient.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NewLife.Web.OAuth
+{
+    /// <summary>微博身份验证提供者</summary>
+    public class WeiboClient : OAuthClient
+    {
+        /// <summary>实例化</summary>
+        public WeiboClient()
+        {
+            var url = "https://api.weibo.com/oauth2/";
+
+            AuthUrl = url + "authorize?response_type={response_type}&client_id={key}&redirect_uri={redirect}&state={state}&scope={scope}";
+            AccessUrl = "#" + url + "access_token?grant_type=authorization_code&client_id={key}&client_secret={secret}&code={code}&state={state}&redirect_uri={redirect}";
+            UserUrl = "https://api.weibo.com/2/users/show.json?uid={userid}&access_token={token}";
+        }
+
+        /// <summary>从响应数据中获取信息</summary>
+        /// <param name="dic"></param>
+        protected override void OnGetInfo(IDictionary<String, String> dic)
+        {
+            base.OnGetInfo(dic);
+
+            if (dic.TryGetValue("screen_name", out var str)) NickName = str.Trim();
+            if (dic.TryGetValue("gender", out str)) Sex = str == "m" ? 1 : (str == "f" ? 2 : 0);
+            if (dic.TryGetValue("description", out str)) Detail = str.Trim();
+
+            if (Avatar.IsNullOrEmpty() && dic.TryGetValue("avatar_hd", out str)) Avatar = str.Trim();
+            if (Avatar.IsNullOrEmpty() && dic.TryGetValue("avatar_large", out str)) Avatar = str.Trim();
+            if (Avatar.IsNullOrEmpty() && dic.TryGetValue("profile_image_url", out str)) Avatar = str.Trim();
+        }
+    }
+}
