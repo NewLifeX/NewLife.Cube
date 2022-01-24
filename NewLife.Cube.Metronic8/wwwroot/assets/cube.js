395968661eddb32b734460d9947658e3f5c1507d
Xusp <djswcn@gmail.com>
2022-01-24 12:00:32 +0800

    修复因升级Metronic 8导致日期选择框不能正确弹出的问题。

--- NewLife.Cube.Metronic8/wwwroot/assets/cube.js
+++ NewLife.Cube.Metronic8/wwwroot/assets/cube.js
@@ -43,104 +43,12 @@
         });
 
     //时间
-    $(".form_datetime").daterangepicker({
-        singleDatePicker: true,
-        showDropdowns: true,
-        autoUpdateInput: false,
-        locale: {
-            format: "YYYY-MM-DD",
-            applyLabel: '确定',
-            cancelLabel: '取消',
-            daysOfWeek: ['日', '一', '二', '三', '四', '五', '六'],
-            monthNames: ['一月', '二月', '三月', '四月', '五月', '六月',
-                '七月', '八月', '九月', '十月', '十一月', '十二月'
-            ],
-        }
+    $(".form_datetime_hms").flatpickr({
+        dateFormat: "Y-m-d H:i:S",
     });
-    $(".form_datetime_hm").daterangepicker({
-        singleDatePicker: true,
-        showDropdowns: true,
-        autoUpdateInput: false,
-        timePicker: true,
-        timePicker24Hour: true,
-        locale: {
-            format: "YYYY-MM-DD hh:mm",
-            applyLabel: '确定',
-            cancelLabel: '取消',
-            daysOfWeek: ['日', '一', '二', '三', '四', '五', '六'],
-            monthNames: ['一月', '二月', '三月', '四月', '五月', '六月',
-                '七月', '八月', '九月', '十月', '十一月', '十二月'
-            ],
-        }
+    $(".form_datetime").flatpickr({
+        dateFormat : "Y-m-d",
     });
-    $(".form_datetime_hms").daterangepicker({
-        singleDatePicker: true,
-        showDropdowns: true,
-        //autoUpdateInput: false,
-        timePicker: true,
-        timePicker24Hour: true,
-        timePickerSeconds: true,
-        locale: {
-            format: "YYYY-MM-DD hh:mm:ss",
-            applyLabel: '确定',
-            cancelLabel: '取消',
-            daysOfWeek: ['日', '一', '二', '三', '四', '五', '六'],
-            monthNames: ['一月', '二月', '三月', '四月', '五月', '六月',
-                '七月', '八月', '九月', '十月', '十一月', '十二月'
-            ],
-        }
-    });
-    //时间事件
-    $(".form_datetime").on('apply.daterangepicker', function (ev, picker) {
-        $(this).val(picker.startDate.format('YYYY-MM-DD'));
-    });
-    $(".form_datetime_hm").on('apply.daterangepicker', function (ev, picker) {
-        $(this).val(picker.startDate.format('YYYY-MM-DD hh:mm'));
-    });
-    $(".form_datetime_hms").on('apply.daterangepicker', function (ev, picker) {
-        $(this).val(picker.startDate.format('YYYY-MM-DD hh:mm:ss'));
-    });
-    $(".form_datetime,.form_datetime_hm,.form_datetime_hms").on('cancel.daterangepicker', function (ev, picker) {
-        $(this).val('');
-    });
-
-    //双击单元格跳转到form
-    $('tr').dblclick(function () {
-        var $this = $(this);
-        var row = $this.closest("tr");
-        //优先加载侧边栏
-        var dblp = row.find('.dblclickpanel');
-        if (dblp.length > 0) {
-            var id = $(dblp).data("id");
-            var url = $(dblp).data("url");
-            $.ajax({
-                url: url,
-                data: { id: id },
-                success: function (data) {
-                    $("#kt_quick_panel_toggler_btn").trigger("click");
-                    $("#kt_quick_panel_content").html(data);
-                    //$("#kt_quick_panel").css("opacity",1);
-                    console.log(Date.now());
-                }
-            });
-            return;
-        }
-
-        //编辑页面
-        var findcell = row.find('.editcell');
-        if (findcell.length > 0) {
-            window.location.href = findcell.attr("href");
-        }
-    });
-
-    //$('tr').click(function () {
-    //    if ($(this).css('background-color') === 'rgb(255, 228, 225)') {
-    //        $(this).css('background-color', 'white');
-    //    } else {
-    //        $(this).css('background-color', 'rgb(255, 228, 225)');
-    //    }
-
-    //});
 
     //弹出提示对话框
     $('.swal_tip').click(function (e) {
@@ -192,7 +100,6 @@
         }); //设置div绝对定位 坐标就是距离鼠标当前的位置 
     });
 });
-
 
 function doClickAction($this) {
     var fields = $this.data('fields');
