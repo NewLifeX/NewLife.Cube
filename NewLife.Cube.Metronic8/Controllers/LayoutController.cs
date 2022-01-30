c2531fa311768c71f393c11bf49f83f20d2f531c
Xusp <djswcn@hotmail.com>
2022-01-30 22:18:59 +0800

    优化用户信息，编辑表单界面。

---
+++ NewLife.Cube.Metronic8/Controllers/LayoutController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using XCode.Membership;
+
+namespace NewLife.Cube.Metronic8.Controllers
+{
+    /// <summary>
+    /// 界面布局控制器，主要用于保存魔方界面状态
+    /// </summary>
+    /// <seealso cref="Microsoft.AspNetCore.Mvc.Controller" />
+    public class LayoutController : Controller
+    {
+        /// <summary>
+        /// 保存界面状态
+        /// </summary>
+        /// <param name="para">The para.</param>
+        /// <returns></returns>
+        public EmptyResult SaveLayout(Parameter para) 
+        { 
+            if(para != null)
+                para.Save();
+
+            return new EmptyResult();   
+        }
+    }
+}
