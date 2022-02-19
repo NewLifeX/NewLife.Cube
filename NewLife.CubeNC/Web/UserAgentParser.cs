29df94cfc090b790de628d88b45bf557ce4b367e
大石头 <nnhy@vip.qq.com>
2022-02-19 10:36:51 +0800

    新增UserAgentParser，用于分析浏览器特性

---
+++ NewLife.CubeNC/Web/UserAgentParser.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace NewLife.Cube.Web;
+
+/// <summary>
+/// 浏览器特性分析器
+/// </summary>
+public class UserAgentParser
+{
+    #region 属性
+    /// <summary>
+    /// 兼容性，一般是 Mozilla/5.0
+    /// </summary>
+    public String Compatible { get; set; }
+
+    /// <summary>平台</summary>
+    public String Platform { get; set; }
+
+    /// <summary>加密特性</summary>
+    public String Encryption { get; set; }
+
+    /// <summary>系统或处理器</summary>
+    public String OSorCPU { get; set; }
+
+    /// <summary>设备</summary>
+    public String Device { get; set; }
+
+    /// <summary>设备编译版本</summary>
+    public String DeviceBuild { get; set; }
+
+    /// <summary>用户浏览器</summary>
+    public String Brower { get; set; }
+
+    /// <summary>移动版本</summary>
+    public String Mobile { get; set; }
+    #endregion
+
+    #region 方法
+    private static readonly Regex _regex = new(@"([^\s\(\)]+)\s*(\([^\(\)]+\))?");
+    /// <summary>
+    /// 分析浏览器UserAgent字符串
+    /// </summary>
+    /// <param name="userAgent"></param>
+    /// <returns></returns>
+    public Boolean Parse(String userAgent)
+    {
+        if (userAgent.IsNullOrEmpty()) return false;
+
+        var ms = _regex.Matches(userAgent);
+
+        var count = ms.Count;
+        if (count == 0) return false;
+
+        // 首先识别主流浏览器，不同浏览器格式不同
+        var infos = ms.Select(e => e.Value?.Trim()).ToArray();
+        ParseChrome(infos);
+
+        // 其它浏览器
+        if (Brower.IsNullOrEmpty()) ParseOtherBrowser(infos);
+
+        // 移动
+        var inf = infos.FirstOrDefault(e => e.StartsWithIgnoreCase("Mobile/"));
+        if (inf != null) Mobile = inf.Trim();
+
+        {
+            // 识别操作系统平台
+            var match = ms[0];
+            Compatible = match.Groups[1].Value;
+
+            // Mozilla/MozillaVersion (Platform; Encryption; OS-or-CPU; Language; PrereleaseVersi
+            var ss = match.Groups[2].Value?.Trim('(', ')').Split(';');
+            if (ss != null && ss.Length > 0)
+            {
+                if (ss.Length >= 4)
+                {
+                    Platform = ss[0]?.Trim();
+                    Encryption = ss[1]?.Trim();
+                    OSorCPU = ss[2]?.Trim().TrimStart("CPU ");
+                    Device = ss[3]?.Trim();
+                }
+                else if (ss.Length >= 2)
+                {
+                    Platform = ss[0]?.Trim();
+                    OSorCPU = ss[1]?.Trim().TrimStart("CPU ");
+                }
+
+                if (!OSorCPU.IsNullOrEmpty())
+                {
+                    var p = OSorCPU.IndexOf("like");
+                    if (p >= 0) OSorCPU = OSorCPU[..p].Trim();
+                }
+
+                if (!Device.IsNullOrEmpty())
+                {
+                    var p = Device.IndexOf("Build/");
+                    if (p >= 0)
+                    {
+                        DeviceBuild = Device[p..].Trim();
+                        Device = Device[..p].Trim();
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+
+    private void ParseChrome(String[] infos)
+    {
+        var inf = infos.FirstOrDefault(e => e.StartsWith("Chrome/"));
+        if (inf == null) return;
+
+        Brower = inf;
+    }
+
+    private void ParseOtherBrowser(String[] infos)
+    {
+        var list = infos.Where(e => !e.Contains("(") && !e.StartsWithIgnoreCase("AppleWebKit/", "Chrome/", "Safari/", "Mobile/", "Version/")).ToList();
+        if (list.Count == 0)
+        {
+            // 最后识别Safari，别人都仿它
+            var inf = infos.FirstOrDefault(e => e.StartsWithIgnoreCase("Safari/"));
+            if (inf != null)
+            {
+                Brower = inf.Trim();
+
+                // 合并版本
+                inf = infos.FirstOrDefault(e => e.StartsWithIgnoreCase("Version/"));
+                if (inf != null)
+                    Brower = Brower.Split('/')[0] + "/" + inf.Split('/')[^1];
+            }
+
+            return;
+        }
+
+        Brower = list[0];
+    }
+    #endregion
+}
---
+++ XUnitTest/UserAgentParserTests.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NewLife.Cube.Web;
+using Xunit;
+
+namespace XUnitTest
+{
+    public class UserAgentParserTests
+    {
+        [Theory]
+        [InlineData("Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36")]
+        [InlineData("Mozilla/5.0 (iPhone; CPU iPhone OS 5_1_1 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9B206 Safari/7534.48.3")]
+        [InlineData("Mozilla/5.0 (Linux; U; Android 7.0;m2 note Build/LMY47D) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/5.0.2 MQQBrowser/6.7 Mobile/15A372 Safari/537.36")]
+        public void Parse(String userAgent)
+        {
+            var ua = new UserAgentParser();
+            var rs = ua.Parse(userAgent);
+            Assert.True(rs);
+            //Assert.Equal(userAgent, ua.Brower);
+        }
+
+        [Fact]
+        public void Parse2()
+        {
+            var userAgent = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36";
+            var ua = new UserAgentParser();
+            ua.Parse(userAgent);
+
+            Assert.Equal("Mozilla/5.0", ua.Compatible);
+            Assert.Equal("Windows NT 10.0", ua.Platform);
+            Assert.Equal("WOW64", ua.OSorCPU);
+            Assert.Equal("Chrome/86.0.4240.198", ua.Brower);
+        }
+
+        [Fact]
+        public void Parse3()
+        {
+            var userAgent = "Mozilla/5.0 (iPhone; CPU iPhone OS 5_1_1 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9B206 Safari/7534.48.3";
+            var ua = new UserAgentParser();
+            ua.Parse(userAgent);
+
+            Assert.Equal("Mozilla/5.0", ua.Compatible);
+            Assert.Equal("iPhone", ua.Platform);
+            Assert.Equal("iPhone OS 5_1_1", ua.OSorCPU);
+            Assert.Equal("Safari/5.1", ua.Brower);
+        }
+
+        [Fact]
+        public void Parse4()
+        {
+            var userAgent = "Mozilla/5.0 (Linux; U; Android 7.0;m2 note Build/LMY47D) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/5.0.2 MQQBrowser/6.7 Mobile/15A372 Safari/537.36";
+            var ua = new UserAgentParser();
+            ua.Parse(userAgent);
+
+            Assert.Equal("Mozilla/5.0", ua.Compatible);
+            Assert.Equal("Linux", ua.Platform);
+            Assert.Equal("U", ua.Encryption);
+            Assert.Equal("Android 7.0", ua.OSorCPU);
+            Assert.Equal("m2 note", ua.Device);
+            Assert.Equal("Build/LMY47D", ua.DeviceBuild);
+            Assert.Equal("MQQBrowser/6.7", ua.Brower);
+            Assert.Equal("Mobile/15A372", ua.Mobile);
+        }
+    }
+}
