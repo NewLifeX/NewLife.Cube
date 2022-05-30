931f055614562a8cafc26b39461d2f53fa9fb66f
青青河边草 <421643133@qq.com>
2022-05-30 10:47:33 +0800

    调整layuiadmin资源文件

--- NewLife.Cube.LayuiAdmin/wwwroot/assets/js/custom/documentation/general/typed.js
+++
@@ -1,1 +0,0 @@
-"use strict";var KTGeneralTypedJsDemos={init:function(){new Typed("#kt_typedjs_example_1",{strings:["First sentence.","Second sentence.","Third sentense","And some longer sentence"],typeSpeed:30})}};KTUtil.onDOMContentLoaded((function(){KTGeneralTypedJsDemos.init()}));
