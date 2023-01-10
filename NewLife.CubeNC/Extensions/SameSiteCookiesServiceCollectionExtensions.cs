36f9815e5e2a4f37da14b48eb4797e35659a55b5
大石头 <nnhy@vip.qq.com>
2023-01-10 20:51:29 +0800

    代码整理

--- NewLife.CubeNC/Extensions/SameSiteCookiesServiceCollectionExtensions.cs
+++ NewLife.CubeNC/Extensions/SameSiteCookiesServiceCollectionExtensions.cs
@@ -1,67 +1,62 @@
-﻿using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
-using System;
+﻿namespace Microsoft.Extensions.DependencyInjection;
 
-namespace Microsoft.Extensions.DependencyInjection
+/// <summary>
+/// Chrome80调整SameSite策略导致Cookie问题的处理方案
+/// 参考https://bbs.huaweicloud.com/blogs/156450
+/// </summary>
+public static class SameSiteCookiesServiceCollectionExtensions
 {
+    private const SameSiteMode Unspecified = (SameSiteMode)(-1);
+
     /// <summary>
-    /// Chrome80调整SameSite策略导致Cookie问题的处理方案
-    /// 参考https://bbs.huaweicloud.com/blogs/156450
+    /// 配置SameSite
     /// </summary>
-    public static class SameSiteCookiesServiceCollectionExtensions
+    /// <param name="services"></param>
+    /// <returns></returns>
+    public static IServiceCollection ConfigureNonBreakingSameSiteCookies(this IServiceCollection services)
     {
-        private const SameSiteMode Unspecified = (SameSiteMode)(-1);
-
-        /// <summary>
-        /// 配置SameSite
-        /// </summary>
-        /// <param name="services"></param>
-        /// <returns></returns>
-        public static IServiceCollection ConfigureNonBreakingSameSiteCookies(this IServiceCollection services)
+        services.Configure<CookiePolicyOptions>(options =>
         {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                options.MinimumSameSitePolicy = Unspecified;
-                options.OnAppendCookie = cookieContext =>
-                   CheckSameSite(cookieContext.Context, cookieContext.CookieOptions);
-                options.OnDeleteCookie = cookieContext =>
-                   CheckSameSite(cookieContext.Context, cookieContext.CookieOptions);
-            });
+            options.MinimumSameSitePolicy = Unspecified;
+            options.OnAppendCookie = cookieContext =>
+               CheckSameSite(cookieContext.Context, cookieContext.CookieOptions);
+            options.OnDeleteCookie = cookieContext =>
+               CheckSameSite(cookieContext.Context, cookieContext.CookieOptions);
+        });
 
-            return services;
-        }
+        return services;
+    }
 
-        private static void CheckSameSite(HttpContext httpContext, CookieOptions options)
+    private static void CheckSameSite(HttpContext httpContext, CookieOptions options)
+    {
+        if (options.SameSite == SameSiteMode.None)
         {
-            if (options.SameSite == SameSiteMode.None)
-            {
-                var userAgent = httpContext.Request.Headers["User-Agent"].ToString();
+            var userAgent = httpContext.Request.Headers["User-Agent"].ToString();
 
-                if (DisallowsSameSiteNone(userAgent))
-                {
-                    options.SameSite = Unspecified;
-                }
+            if (DisallowsSameSiteNone(userAgent))
+            {
+                options.SameSite = Unspecified;
             }
         }
+    }
 
-        private static Boolean DisallowsSameSiteNone(String userAgent)
+    private static Boolean DisallowsSameSiteNone(String userAgent)
+    {
+        if (userAgent.Contains("CPU iPhone OS 12") || userAgent.Contains("iPad; CPU OS 12"))
         {
-            if (userAgent.Contains("CPU iPhone OS 12") || userAgent.Contains("iPad; CPU OS 12"))
-            {
-                return true;
-            }
-
-            if (userAgent.Contains("Safari") && userAgent.Contains("Macintosh; Intel Mac OS X 10_14") && userAgent.Contains("Version/"))
-            {
-                return true;
-            }
-
-            if (userAgent.Contains("Chrome/5") || userAgent.Contains("Chrome/6"))
-            {
-                return true;
-            }
-
-            return false;
+            return true;
         }
+
+        if (userAgent.Contains("Safari") && userAgent.Contains("Macintosh; Intel Mac OS X 10_14") && userAgent.Contains("Version/"))
+        {
+            return true;
+        }
+
+        if (userAgent.Contains("Chrome/5") || userAgent.Contains("Chrome/6"))
+        {
+            return true;
+        }
+
+        return false;
     }
 }
