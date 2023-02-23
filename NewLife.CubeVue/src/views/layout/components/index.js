d790d02f9f5fd26df0f378ad1dc76c9b9079f776
大石头 <nnhy@vip.qq.com>
2023-02-23 13:54:49 +0800

    删除不再使用的文件

--- NewLife.CubeVue/src/views/layout/components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Navbar } from '@/views/layout/components/navbar'
-export { default as Sidebar } from '@/views/layout/components/sidebar/index'
-// export { default as TagsView } from '@/views/layout/components/TagsView'
-export { default as AppMain } from '@/views/layout/components/appMain'
