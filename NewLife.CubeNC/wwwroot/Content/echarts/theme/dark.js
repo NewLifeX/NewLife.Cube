7c81b57fdf2c9b8aa94ab39d06f0552a638ff789
Xusp <djswcn@hotmail.com>
2023-07-15 15:04:52 +0800

    升级echarts到5.4.2。

--- NewLife.CubeNC/wwwroot/Content/echarts/theme/dark.js
+++ NewLife.CubeNC/wwwroot/Content/echarts/theme/dark.js
@@ -1,16 +1,38 @@
-(function (root, factory) {
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+(function(root, factory) {
     if (typeof define === 'function' && define.amd) {
         // AMD. Register as an anonymous module.
         define(['exports', 'echarts'], factory);
-    } else if (typeof exports === 'object' && typeof exports.nodeName !== 'string') {
+    } else if (
+        typeof exports === 'object' &&
+        typeof exports.nodeName !== 'string'
+    ) {
         // CommonJS
-        factory(exports, require('echarts'));
+        factory(exports, require('echarts/lib/echarts'));
     } else {
         // Browser globals
         factory({}, root.echarts);
     }
-}(this, function (exports, echarts) {
-    var log = function (msg) {
+})(this, function(exports, echarts) {
+    var log = function(msg) {
         if (typeof console !== 'undefined') {
             console && console.error && console.error(msg);
         }
@@ -19,7 +41,8 @@
         log('ECharts is not Loaded');
         return;
     }
-    var contrastColor = '#eee';
+    var contrastColor = '#B9B8CE';
+    var backgroundColor = '#100C2A';
     var axisCommon = function () {
         return {
             axisLine: {
@@ -27,42 +50,50 @@
                     color: contrastColor
                 }
             },
-            axisTick: {
-                lineStyle: {
-                    color: contrastColor
-                }
-            },
-            axisLabel: {
-                textStyle: {
-                    color: contrastColor
-                }
-            },
             splitLine: {
                 lineStyle: {
-                    type: 'dashed',
-                    color: '#aaa'
+                    color: '#484753'
                 }
             },
             splitArea: {
                 areaStyle: {
-                    color: contrastColor
+                    color: ['rgba(255,255,255,0.02)', 'rgba(255,255,255,0.05)']
+                }
+            },
+            minorSplitLine: {
+                lineStyle: {
+                    color: '#20203B'
                 }
             }
         };
     };
 
-    var colorPalette = ['#dd6b66','#759aa0','#e69d87','#8dc1a9','#ea7e53','#eedd78','#73a373','#73b9bc','#7289ab', '#91ca8c','#f49f42'];
+    var colorPalette = [
+        '#4992ff',
+        '#7cffb2',
+        '#fddd60',
+        '#ff6e76',
+        '#58d9f9',
+        '#05c091',
+        '#ff8a45',
+        '#8d48e3',
+        '#dd79ff'
+    ];
     var theme = {
+        darkMode: true,
+
         color: colorPalette,
-        backgroundColor: '#333',
-        tooltip: {
-            axisPointer: {
-                lineStyle: {
-                    color: contrastColor
-                },
-                crossStyle: {
-                    color: contrastColor
-                }
+        backgroundColor: backgroundColor,
+        axisPointer: {
+            lineStyle: {
+                color: '#817f91'
+            },
+            crossStyle: {
+                color: '#817f91'
+            },
+            label: {
+                // TODO Contrast of label backgorundColor
+                color: '#fff'
             }
         },
         legend: {
@@ -75,17 +106,63 @@
         },
         title: {
             textStyle: {
-                color: contrastColor
+                color: '#EEF1FA'
+            },
+            subtextStyle: {
+                color: '#B9B8CE'
             }
         },
         toolbox: {
             iconStyle: {
-                normal: {
-                    borderColor: contrastColor
-                }
+                borderColor: contrastColor
             }
         },
         dataZoom: {
+            borderColor: '#71708A',
+            textStyle: {
+                color: contrastColor
+            },
+            brushStyle: {
+                color: 'rgba(135,163,206,0.3)'
+            },
+            handleStyle: {
+                color: '#353450',
+                borderColor: '#C5CBE3'
+            },
+            moveHandleStyle: {
+                color: '#B0B6C3',
+                opacity: 0.3
+            },
+            fillerColor: 'rgba(135,163,206,0.2)',
+            emphasis: {
+                handleStyle: {
+                    borderColor: '#91B7F2',
+                    color: '#4D587D'
+                },
+                moveHandleStyle: {
+                    color: '#636D9A',
+                    opacity: 0.7
+                }
+            },
+            dataBackground: {
+                lineStyle: {
+                    color: '#71708A',
+                    width: 1
+                },
+                areaStyle: {
+                    color: '#71708A'
+                }
+            },
+            selectedDataBackground: {
+                lineStyle: {
+                    color: '#87A3CE'
+                },
+                areaStyle: {
+                    color: '#87A3CE'
+                }
+            }
+        },
+        visualMap: {
             textStyle: {
                 color: contrastColor
             }
@@ -94,23 +171,26 @@
             lineStyle: {
                 color: contrastColor
             },
-            itemStyle: {
-                normal: {
-                    color: colorPalette[1]
-                }
-            },
             label: {
-                normal: {
-                    textStyle: {
-                        color: contrastColor
-                    }
-                }
+                color: contrastColor
             },
             controlStyle: {
-                normal: {
-                    color: contrastColor,
-                    borderColor: contrastColor
-                }
+                color: contrastColor,
+                borderColor: contrastColor
+            }
+        },
+        calendar: {
+            itemStyle: {
+                color: backgroundColor
+            },
+            dayLabel: {
+                color: contrastColor
+            },
+            monthLabel: {
+                color: contrastColor
+            },
+            yearLabel: {
+                color: contrastColor
             }
         },
         timeAxis: axisCommon(),
@@ -126,22 +206,19 @@
         },
         gauge: {
             title: {
-                textStyle: {
-                    color: contrastColor
-                }
+                color: contrastColor
             }
         },
         candlestick: {
             itemStyle: {
-                normal: {
-                    color: '#FD1050',
-                    color0: '#0CF49B',
-                    borderColor: '#FD1050',
-                    borderColor0: '#0CF49B'
-                }
+                color: '#FD1050',
+                color0: '#0CF49B',
+                borderColor: '#FD1050',
+                borderColor0: '#0CF49B'
             }
         }
     };
+
     theme.categoryAxis.splitLine.show = false;
     echarts.registerTheme('dark', theme);
-}));
+});
