4f8b45597f2a2ec653085aae22f1a5529097e62f
Xusp <djswcn@hotmail.com>
2023-07-22 23:15:20 +0800

    优化列表视图显示效果。

--- NewLife.CubeNC/Areas/Admin/Controllers/DepartmentController.cs
+++ NewLife.CubeNC/Areas/Admin/Controllers/DepartmentController.cs
@@ -18,7 +18,7 @@ namespace NewLife.Cube.Admin.Controllers
         {
             LogOnChange = true;
 
-            ListFields.RemoveField("Ex1", "Ex2", "Ex3", "Ex4", "Ex5", "Ex6");
+            ListFields.RemoveField("Id", "TenantId", "Ex1", "Ex2", "Ex3", "Ex4", "Ex5", "Ex6");
             ListFields.RemoveUpdateField();
             ListFields.RemoveCreateField();
             ListFields.RemoveRemarkField();
