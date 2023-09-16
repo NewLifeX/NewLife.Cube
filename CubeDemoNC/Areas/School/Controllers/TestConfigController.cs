039efb242186f82931078025e7acf4de870e913e
xiyunfei <xi3892@163.com>
2023-09-16 11:12:58 +0800

    调整配置类界面模板页的tab标签默认值

---
+++ CubeDemoNC/Areas/School/Controllers/TestConfigController.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+using IoTServer;
+using NewLife.Cube;
+
+namespace CubeDemo.Areas.School.Controllers
+{
+    /// <summary></summary>
+    [SchoolArea]
+    [DisplayName("测试配置")]
+    public class TestConfigController : ConfigController<IoTSetting>
+    {
+    }
+}
