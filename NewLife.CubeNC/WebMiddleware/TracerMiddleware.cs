25a734869eb03a14255a42d381cfef174b57e249
大石头 <nnhy@vip.qq.com>
2024-04-25 11:32:58 +0800

    借助ISpan数值标签记录请求响应大小

--- NewLife.CubeNC/WebMiddleware/TracerMiddleware.cs
+++ NewLife.CubeNC/WebMiddleware/TracerMiddleware.cs
@@ -1,6 +1,5 @@
 ﻿using System.Net;
 using NewLife.Log;
-using NewLife.Remoting;
 using NewLife.Web;
 using HttpContext = Microsoft.AspNetCore.Http.HttpContext;
 
@@ -23,7 +22,7 @@ public class TracerMiddleware
     /// <returns></returns>
     public async Task Invoke(HttpContext ctx)
     {
-        // APM跟踪
+        //!! 以下代码不能封装为独立方法，因为有异步存在，代码被拆分为状态机，导致这里建立的埋点span无法关联页面接口内的下级埋点
         ISpan span = null;
         if (Tracer != null && !ctx.WebSockets.IsWebSocketRequest)
         {
@@ -36,27 +35,35 @@ public class TracerMiddleware
                 span = Tracer.NewSpan(action);
                 span.Tag = $"{ctx.GetUserHost()} {req.Method} {req.GetRawUrl()}";
                 span.Detach(req.Headers);
+                span.Value = req.ContentLength ?? 0;
                 if (span is DefaultSpan ds && ds.TraceFlag > 0)
                 {
-                    if (req.ContentLength != null && req.ContentLength < 1024 * 8)
+                    var flag = false;
+                    if (req.ContentLength != null &&
+                        req.ContentLength < 1024 * 8 &&
+                        req.ContentType != null &&
+                        req.ContentType.StartsWithIgnoreCase(TagTypes))
                     {
                         req.EnableBuffering();
 
                         var buf = new Byte[1024];
                         var count = await req.Body.ReadAsync(buf, 0, buf.Length);
-                        span.Tag = Environment.NewLine + buf.ToStr(null, 0, count);
+                        span.AppendTag("\r\n<=\r\n" + buf.ToStr(null, 0, count));
                         req.Body.Position = 0;
-                    }
-                    else
-                    {
-                        span.AppendTag($"ContentLength: {req.ContentLength}");
-                        span.AppendTag($"ContentType: {req.ContentType}");
+                        flag = true;
                     }
 
                     if (span.Tag.Length < 500)
                     {
+                        if (!flag) span.AppendTag("\r\n<=");
                         var vs = req.Headers.Where(e => !e.Key.EqualIgnoreCase(ExcludeHeaders)).ToDictionary(e => e.Key, e => e.Value + "");
-                        span.Tag += Environment.NewLine + vs.Join(Environment.NewLine, e => $"{e.Key}:{e.Value}");
+                        span.AppendTag("\r\n" + vs.Join(Environment.NewLine, e => $"{e.Key}:{e.Value}"));
+                    }
+                    else if (!flag)
+                    {
+                        span.AppendTag("\r\n<=\r\n");
+                        span.AppendTag($"ContentLength: {req.ContentLength}\r\n");
+                        span.AppendTag($"ContentType: {req.ContentType}");
                     }
                 }
             }
@@ -72,20 +79,58 @@ public class TracerMiddleware
             // 根据状态码识别异常
             if (span != null)
             {
-                var code = ctx.Response.StatusCode;
-                if (code >= 400 && code != 404) span.SetError(new HttpRequestException($"Http Error {code} {(HttpStatusCode)code}"), null);
+                var res = ctx.Response;
+                span.Value += res.ContentLength ?? 0;
+                var code = res.StatusCode;
+                if (code >= 400 && code != 404)
+                    span.SetError(new HttpRequestException($"Http Error {code} {(HttpStatusCode)code}"), null);
+                else if (code == 200)
+                {
+                    if (span is DefaultSpan ds && ds.TraceFlag > 0 && (span.Tag == null || span.Tag.Length < 500))
+                    {
+                        var flag = false;
+                        if (res.ContentLength != null &&
+                            res.ContentLength < 1024 * 8 &&
+                            res.Body.CanSeek &&
+                            res.ContentType != null &&
+                            res.ContentType.StartsWithIgnoreCase(TagTypes))
+                        {
+                            var buf = new Byte[1024];
+                            var p = res.Body.Position;
+                            var count = await res.Body.ReadAsync(buf, 0, buf.Length);
+                            span.AppendTag("\r\n=>\r\n" + buf.ToStr(null, 0, count));
+                            res.Body.Position = p;
+                            flag = true;
+                        }
+
+                        if (span.Tag == null || span.Tag.Length < 500)
+                        {
+                            if (!flag) span.AppendTag("\r\n=>");
+                            var vs = res.Headers.Where(e => !e.Key.EqualIgnoreCase(ExcludeHeaders)).ToDictionary(e => e.Key, e => e.Value + "");
+                            span.AppendTag("\r\n" + vs.Join(Environment.NewLine, e => $"{e.Key}:{e.Value}"));
+                        }
+                        else if (!flag)
+                        {
+                            span.AppendTag("\r\n=>\r\n");
+                            span.AppendTag($"ContentLength: {res.ContentLength}\r\n");
+                            span.AppendTag($"ContentType: {res.ContentType}");
+                        }
+                    }
+                }
             }
         }
         catch (Exception ex)
         {
-            if (span != null)
-            {
-                // 接口抛出ApiException时，认为是正常业务行为，埋点不算异常
-                if (ex is ApiException)
-                    span.Tag ??= ex.Message;
-                else
-                    span.SetError(ex, null);
-            }
+            //if (span != null)
+            //{
+            //    // 接口抛出ApiException时，认为是正常业务行为，埋点不算异常
+            //    if (ex is ApiException)
+            //        span.Tag ??= ex.Message;
+            //    else
+            //        span.SetError(ex, null);
+            //}
+            // 捕获所有未处理异常，即使是ApiException，也应该在接口层包装而不是继续向外抛出异常
+            span?.SetError(ex, null);
 
             throw;
         }
@@ -95,18 +140,21 @@ public class TracerMiddleware
         }
     }
 
+    /// <summary>支持作为标签数据的内容类型</summary>
+    public static String[] TagTypes { get; set; } = [
+        "text/plain", "text/xml", "application/json", "application/xml", "application/x-www-form-urlencoded"
+    ];
+
     /// <summary>忽略的头部</summary>
-    public static String[] ExcludeHeaders { get; set; } = new[] {
-        "traceparent", "Authorization", "Cookie"
-    };
+    public static String[] ExcludeHeaders { get; set; } = ["traceparent", "Authorization", "Cookie"];
 
     /// <summary>忽略的后缀</summary>
-    public static String[] ExcludeSuffixes { get; set; } = new[] {
+    public static String[] ExcludeSuffixes { get; set; } = [
         ".html", ".htm", ".js", ".css", ".map", ".png", ".jpg", ".gif", ".ico",  // 脚本样式图片
         ".woff", ".woff2", ".svg", ".ttf", ".otf", ".eot"   // 字体
-    };
+    ];
 
-    private static String[] CubeActions = new[] { "index", "detail", "add", "edit", "delete", "deleteSelect", "deleteAll", "ExportCsv", "Info", "SetEnable", "EnableSelect", "DisableSelect", "DeleteSelect" };
+    private static String[] CubeActions = ["index", "detail", "add", "edit", "delete", "deleteSelect", "deleteAll", "ExportCsv", "Info", "SetEnable", "EnableSelect", "DisableSelect", "DeleteSelect"];
     private static String GetAction(HttpContext ctx)
     {
         var p = ctx.Request.Path + "";
