12c4810a46fb449832a2282a4004d30107e99932
猿人易 <qcjxberin@outlook.com>
2024-05-06 13:14:10 +0800

    调整定时作业分布式锁Key名称规则

--- NewLife.CubeNC/Services/JobService.cs
+++ NewLife.CubeNC/Services/JobService.cs
@@ -269,7 +269,7 @@ internal class MyJob : IDisposable
     private Boolean CheckRunning(CronJob job)
     {
         // 检查分布式锁，避免多节点重复执行
-        var key = $"{SysConfig.Current.Name}:Job:{job.Id}";
+        var key = $"Job:{SysConfig.Current.Name}:{job.Id}";
         if (CacheProvider != null && !CacheProvider.Cache.Add(key, job.Name, 5)) return false;
 
         // 有时候可能并没有配置Redis，借助数据库事务实现去重，需要20230804版本的XCode
