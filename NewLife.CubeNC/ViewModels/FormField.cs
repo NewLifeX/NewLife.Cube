60828284b5377e5f7ed3fc7e426ecc6093691ae1
智能大石头 <nnhy@vip.qq.com>
2024-09-26 23:59:14 +0800

    GroupView不要.cshtml后缀

--- NewLife.CubeNC/ViewModels/FormField.cs
+++ NewLife.CubeNC/ViewModels/FormField.cs
@@ -9,7 +9,7 @@ public delegate Object GetExpandDelegate(Object entity);
 public class FormField : DataField
 {
 #if MVC
-    /// <summary>MVC特有，表单字段的分部视图名称。对标_Form_Group，允许针对字段定义视图</summary>
+    /// <summary>MVC特有，表单字段的分部视图名称，不要.cshtml后缀。对标_Form_Group，允许针对字段定义视图</summary>
     public String GroupView { get; set; }
 
     /// <summary>获取扩展字段委托。当前字段所表示的对象，各属性作为表单字段展开</summary>
