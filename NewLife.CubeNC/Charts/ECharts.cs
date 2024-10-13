0d5c8db91483a915379257b6ed9e7116c12c882b
大石头 <nnhy@vip.qq.com>
2024-10-13 14:04:47 +0800

    Upgrade Nuget

--- NewLife.CubeNC/Charts/ECharts.cs
+++ NewLife.CubeNC/Charts/ECharts.cs
@@ -487,8 +487,8 @@ public class ECharts : IExtend
     {
         var keyName = field.Category;
         keyName ??= _xField as String;
-        keyName ??= (_xField as DataField).Name;
-        keyName ??= (_xField as FieldItem).Name;
+        keyName ??= (_xField as DataField)?.Name;
+        keyName ??= (_xField as FieldItem)?.Name;
         var sr = new Series
         {
             Name = field.DisplayName ?? field.Name,
@@ -509,8 +509,8 @@ public class ECharts : IExtend
     public Series AddPie<T>(IList<T> list, FieldItem field, Func<T, NameValue> selector = null) where T : IModel
     {
         var keyName = _xField as String;
-        keyName ??= (_xField as DataField).Name;
-        keyName ??= (_xField as FieldItem).Name;
+        keyName ??= (_xField as DataField)?.Name;
+        keyName ??= (_xField as FieldItem)?.Name;
         keyName ??= field.Table.Master?.Name ?? field.Table.PrimaryKeys.FirstOrDefault()?.Name;
         var sr = new Series
         {
