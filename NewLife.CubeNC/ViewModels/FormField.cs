3e2891d3a3a1206c01b809f07cbf8ef0c4717ba9
大石头 <nnhy@vip.qq.com>
2024-11-05 23:24:26 +0800

    ReadOnlyEntityController提取共性代码，MVC与API共用于ReadOnlyEntityController，减少维护工作量

--- NewLife.CubeNC/ViewModels/FormField.cs
+++ NewLife.CubeNC/ViewModels/FormField.cs
@@ -11,26 +11,11 @@ public class FormField : DataField
 #if MVC
     /// <summary>MVC特有，表单字段的分部视图名称，不要.cshtml后缀。对标_Form_Group，允许针对字段定义视图</summary>
     public String GroupView { get; set; }
+#endif
 
     /// <summary>获取扩展字段委托。当前字段所表示的对象，各属性作为表单字段展开</summary>
     public GetExpandDelegate GetExpand { get; set; }
 
     /// <summary>保留扩展字段。默认false，字段被扩展以后，表单上就不再出现原字段</summary>
     public Boolean RetainExpand { get; set; }
-#endif
-
-    #region 方法
-    ///// <summary>克隆</summary>
-    ///// <returns></returns>
-    //public override DataField Clone()
-    //{
-    //    var df = base.Clone();
-    //    if (df is FormField ff)
-    //    {
-    //        ff.GroupView = GroupView;
-    //    }
-
-    //    return df;
-    //}
-    #endregion
 }
