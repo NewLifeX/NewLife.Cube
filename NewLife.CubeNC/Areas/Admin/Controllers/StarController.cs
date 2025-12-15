b62878ac224837d216e008b002ea297b49a2f3ff
智能大石头 <nnhy@vip.qq.com>
2025-12-15 16:19:05 +0800

    增加星尘设置管理页面

---
+++ NewLife.CubeNC/Areas/Admin/Controllers/StarController.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Stardust;
+using XCode.Membership;
+
+namespace NewLife.Cube.Areas.Admin.Controllers;
+
+/// <summary>设置控制器</summary>
+[DisplayName("星尘设置")]
+[AdminArea]
+[Menu(0, false, Icon = "fa-bomb")]
+public class StarController : ConfigController<StarSetting>
+{
+    /// <summary>已重载。</summary>
+    /// <param name="filterContext"></param>
+    public override void OnActionExecuting(ActionExecutingContext filterContext)
+    {
+        base.OnActionExecuting(filterContext);
+
+        PageSetting.NavView = "_Object_Nav";
+    }
+}
