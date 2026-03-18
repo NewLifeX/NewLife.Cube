5eaa51e2df3f1598fc1821e455b28c7c7dd4db52
大石头 <nnhy@vip.qq.com>
2026-03-18 17:23:08 +0800

    优化文件下载时的文件名设置逻辑
    原先下载文件时始终使用物理文件名，现在优先使用 att.FileName（如有），否则退回物理文件名。此调整提升了下载文件名的灵活性和准确性。

--- NewLife.CubeNC/Controllers/CubeController.cs
+++ NewLife.CubeNC/Controllers/CubeController.cs
@@ -540,7 +540,7 @@ public class CubeController(IFileStorage fileStorage, TokenService tokenService,
 
         // 开启分段下载
         result.EnableRangeProcessing = true;
-        result.FileDownloadName = Path.GetFileName(filePath);
+        result.FileDownloadName = !att.FileName.IsNullOrEmpty() ? att.FileName : Path.GetFileName(filePath);
 
         return result;
     }
